9a283d200e776fe05768ca91528d77dc3ebcfbbf
Perisso <perezljljose@gmail.com>
2017-02-10 11:52:50 +0100

    index of activities part 1

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,11 +3,20 @@ class ActivitiesController < ApplicationController
   respond_to :html, :js, :json
 
   def index
-    params[:q] ||= Activity.ransack_default
-    @search_q = Activity.search(params[:q])
-    @activities = @search_q.result #.paginate(page: params[:page], per_page: params[:per_page]||15)
 
-    respond_with(@activities)
+    params[:q] ||= Activity.ransack_default
+    params[:day] ||= Activity.includes(:events, :timetables).activities_present(0.day.ago.to_s).minimum(:execution_date).strftime ("%Y-%m-%d")
+    @search_q = Activity.includes(:events, :timetables).search({timetables_execution_date_eq: params[:day] })
+    @activities = @search_q.result #.paginate(page: params[:page], per_page: params[:per_page]||15)
+    @list_days = Activity.includes(:timetables).distinct.activities_present(0.day.ago.to_s).order('timetables.execution_date').pluck('timetables.execution_date')
+    @list_days = @list_days.to_json
+     #@first_day = @list_days.minimum(:execution_date).strftime ("%d/%m/%Y")
+    @day = params[:day]
+    respond_to do |format|
+      format.html
+      format.js
+      #format.json @day
+    end
   end
 
   def show
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,6 +3,13 @@ class Activity < ActiveRecord::Base
   has_many   :events, as: :eventable
   belongs_to :entity
 
+  has_many :timetables, through: :events
+  has_many :addresses, through: :events
+
+  accepts_nested_attributes_for :events
+  accepts_nested_attributes_for :links
+
+  scope :activities_present, ->(day) { where("timetables.execution_date >= ?", day) }
 
   def to_s
     name
