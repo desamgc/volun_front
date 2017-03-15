354951f023791459197c72b87c7020ebe6dcf37e
Perisso <perezljljose@gmail.com>
2017-03-15 15:44:33 +0100

    update volunteer_subscribe_controller

--- app/controllers/rt/volunteer_subscribes_controller.rb
+++ app/controllers/rt/volunteer_subscribes_controller.rb
@@ -10,9 +10,6 @@ class Rt::VolunteerSubscribesController < ApplicationController
 
   def new
     @rt_volunteer_subscribe = Rt::VolunteerSubscribe.new
-    respond_to do |format|
-      format.js
-    end
   end
 
   def edit
