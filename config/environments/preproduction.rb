fd5f5e2aa6445d320c416753ecbd0872dc2876fd
Perisso <perezljljose@gmail.com>
2017-03-21 16:00:43 +0100

    config email

--- config/environments/preproduction.rb
+++ config/environments/preproduction.rb
@@ -59,13 +59,18 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+      :address => "correo.munimadrid.es"
+  }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: Rails.application.secrets.server_name }
-  config.action_mailer.asset_host = "https://voluntariospormadridpre.madrid.es"
+  #config.action_mailer.asset_host = "https://voluntariospormadridpre.madrid.es"
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
