1deeb70ae933ce9bf8e3eb56b2d03890c6b933ca
Perisso <perezljljose@gmail.com>
2017-04-03 14:13:10 +0200

    order by id request_reason

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,10 @@
 module ApplicationHelper
-  
   def home_page?
     return false if user_signed_in?
     # Using path because fullpath yields false negatives since it contains
     # parameters too
     request.path == '/'
   end
-
-  
 
   # if current path is /debates current_path_with_query_params(foo: 'bar') returns /debates?foo=bar
   # notice: if query_params have a param which also exist in current path, it "overrides" (query_params is merged last)
@@ -20,7 +17,7 @@ module ApplicationHelper
     render_options = {
       filter_html:     false,
       hard_wrap:       true,
-      link_attributes: {  target: "_blank" }
+      link_attributes: { target: '_blank' }
     }
     renderer = Redcarpet::Render::HTML.new(render_options)
     extensions = {
@@ -35,35 +32,34 @@ module ApplicationHelper
   end
 
   def districts_select_options
-    District.all.order(name: :asc).collect { |g| [ g.name, g.id ] }
+    District.all.order(name: :asc).collect { |g| [g.name, g.id] }
   end
 
   def request_reasons_select_options
-    ReqReason.all.order(description: :asc).collect { |g| [ g.description, g.id ] }
+    ReqReason.all.order(id: :asc).collect { |g| [g.description, g.id] }
   end
 
   def road_types_select_options
-    RoadType.all.order(name: :asc).collect { |g| [ g.name, g.name] }
+    RoadType.all.order(name: :asc).collect { |g| [g.name, g.name] }
   end
 
   def entity_types_select_options
-    EntityType.all.order(description: :asc).collect { |g| [ g.description, g.id ] }
+    EntityType.all.order(description: :asc).collect { |g| [g.description, g.id] }
   end
 
   def number_types_select_options
-    h = {'Km' => :Km, 'Num' => :Num}
-    return h
+    { 'Km' => :Km, 'Num' => :Num }
   end
 
   def provinces_select_options
-    Province.all.order(name: :asc).collect { |g| [ g.name, g.name ] }
+    Province.all.order(name: :asc).collect { |g| [g.name, g.name] }
   end
 
   def projects_select_options
-    Project.all.order(name: :asc).collect { |g| [ g.name, g.id ] }
+    Project.all.order(name: :asc).collect { |g| [g.name, g.id] }
   end
 
-  def entity_projects_select_options (id)
-    Project.entity_projects(id).order(name: :asc).collect { |g| [ g.name, g.id ] }
-  end  
+  def entity_projects_select_options(id)
+    Project.entity_projects(id).order(name: :asc).collect { |g| [g.name, g.id] }
+  end
 end
