6a113afd6bdbb6cb4ac4c33da6034f31b0b46136
Perisso <perezljljose@gmail.com>
2017-04-20 15:43:09 +0200

    rejection_type_id to nill

--- app/models/concerns/rt_commons.rb
+++ app/models/concerns/rt_commons.rb
@@ -25,12 +25,12 @@ module RtCommons
         request_type: RequestType.where(kind: RequestType.kinds[self.class.model_name.singular]).take,
         req_status_id:  1,
         status_date:  Time.now,
-        req_rejection_type_id:  1,
+        #req_rejection_type_id:  ,
         req_reason_id:  self.request_form != nil ? self.request_form.req_reason_id : nil
         #request_type: RequestType.get_request_form_type(self.rt_extendable_type)
       )
       build_request_form(attributes)
-      
+
     end
 
   end
