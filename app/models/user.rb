5eef498aad7c4002d4e151f4d207b05f25cfd374
Perisso <perezljljose@gmail.com>
2017-04-28 14:19:08 +0200

    user confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable, :registerable #, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :registerable, :confirmable
   belongs_to :loggable, polymorphic: true
   belongs_to :notice_type
 
   scope :count_email, ->(email) { where('email = ?', email).count }
 
 
-  #validates :terms_of_service, acceptance: { allow_nil: false }, on: :create
+  validates :terms_of_service, presence: true, on: :create
 
 end
