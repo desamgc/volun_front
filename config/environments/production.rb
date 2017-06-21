cfd90a498166ee136ceec9fb835fc008a07e6c97
Perisso <perezljljose@gmail.com>
2017-06-21 13:15:02 +0200

    config mail server in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,10 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+      :address => Rails.application.secrets.server_name_mail
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
