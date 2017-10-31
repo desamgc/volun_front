520fd86fe3eb3c309d276b1e7cecfc2ab9225bd9
Perisso <perezljljose@gmail.com>
2017-10-31 13:21:56 +0100

    control de acceso por telefono y correo electronico

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,17 @@ class User < ActiveRecord::Base
   belongs_to :loggable, polymorphic: true
   belongs_to :notice_type
 
+  attr_accessor :username
+
   scope :count_email, (->(email) { where('email = ?', email).count })
 
   validates :terms_of_service, presence: true, on: :create
+
+
+
+  def self.find_for_database_authentication warden_conditions
+    conditions = warden_conditions.dup
+    login = conditions.delete(:username)
+    where(conditions).where(["lower(login) = :value OR lower(email) = :value", {value: login.strip.downcase}]).first
+  end
 end
