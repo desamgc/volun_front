7121c4fe4aaba0cd43c351f58119d68f86aab9d5
Perisso <perezljljose@gmail.com>
2017-12-27 17:08:29 +0100

    corrección del mapa 2

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-Date::MONTHNAMES = [nil,'Enero', 'Febrero', 'Marzo',     'Abril',
+MONTHNAMES = [nil,'Enero', 'Febrero', 'Marzo',     'Abril',
 'Mayo','Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre','Noviembre',
 'Diciembre' ]
